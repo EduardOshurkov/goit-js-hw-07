be87b87ae07b7f08c19636f22b9ad80996bdce53
EduardOshurkov <drd2993@gmail.com>
2022-05-23 16:48:58 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,64 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+
+const galleyImages = galleryItems.map(
+    ({ preview, original, description }) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+            <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>`
+);
+
+gallery.innerHTML = galleyImages.join("");
+
+const onClickOpenImages = (event) => {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+
+    event.preventDefault();
+
+    const imageLink = event.target.dataset.source;
+
+    instance
+        .element()
+        .querySelector('img')
+        .setAttribute("src", imageLink)
+    
+    instance.show();
+};
+
+gallery.addEventListener("click", onClickOpenImages);
+
+const instance = basicLightbox.create(`<img src="" width="800" height="600">`, {
+  closable: true,
+  onShow: () => {
+    document.addEventListener("keydown", closeByEscape);
+  },
+  onClose: (instance) => {
+    instance.element().querySelector("img").setAttribute("src", "");
+
+    document.removeEventListener("keydown", closeByEscape);
+  },
+}); 
+
+function closeByEscape(event) {
+  if (event.code === "Escape") {
+    if (instance.visible()) {
+      instance.close();
+    }
+  }
+}
+
+
